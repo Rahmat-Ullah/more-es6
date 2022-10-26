15550d6d8e05a52247b28284210e050545d04adc
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-10-26 08:21:34 +0600

    for each practiced

---
+++ for-each.js
@@ -0,0 +1,14 @@
+const friends = ['Tom hanks', 'tom cruise', 'tom brady', 'tom solaiman'];
+
+friends.forEach(friend =>{
+
+})
+
+const products = [
+    {id: 1, name: 'laptop', price: 45000},
+    {id: 1, name: 'mobile', price: 22000},
+    {id: 1, name: 'watch', price: 75000},
+    {id: 1, name: 'tablet', price: 23000},
+];
+
+products.forEach(product => console.log(product));
